6e74354daa76b2527ed070a84a36434e5e264069
theambrosial <47419028+theambrosial@users.noreply.github.com>
2021-03-19 20:03:32 +0530

    adding consumer to accept websocket connections

--- wechat/wechat/asgi.py
+++ wechat/wechat/asgi.py
@@ -1,16 +1,10 @@
-"""
-ASGI config for wechat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wechat.settings')
 
@@ -18,5 +12,10 @@ application = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
     # Just HTTP for now. (We can add other protocols later.)
 })
